e726fb53d2e83fb8eb3b56edacb45f51d8e19e52
wangxiaobo33 <wangxiaobo9@jd.com>
2022-01-25 14:00:52 +0800

    feat: 新浪博客数据

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const sinaBlog = require('./src/sina_blog.js')
 const zhihuBlog = require('./src/zhihu.js')
+const weiboBlog = require('./src/weibo.js')
 
-//  sinaBlog(1349490495);
- zhihuBlog('https://zhuanlan.zhihu.com/c_1036201632534478848','黄甦专栏')
+// sinaBlog(1349490495);
+
+// zhihuBlog('https://zhuanlan.zhihu.com/c_1036201632534478848','黄甦专栏')
+
+// zhihuBlog('https://www.zhihu.com/column/investmentclub','交易法门专栏')
+// zhihuBlog('https://www.zhihu.com/column/tradingplan','期货问答专栏')
+
+weiboBlog('caopanshou1') //期货操作手
  
